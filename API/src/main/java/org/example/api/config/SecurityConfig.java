ac36aa3df25e06d2032dbcc8a71608b95c0335de
tzaretskaya <t.zaretskaya@teraplan.ru>
2025-08-13 15:16:30 +0300

    working version

--- API/src/main/java/org/example/api/config/SecurityConfig.java
+++ API/src/main/java/org/example/api/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package org.example.api.config;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +24,7 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
+    http.cors().and()
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers("/reports").hasRole("prothetic_user")
             .anyRequest().authenticated()
@@ -34,19 +40,34 @@ public class SecurityConfig {
 
   @Bean
   public JwtDecoder jwtDecoder() {
-    return NimbusJwtDecoder.withJwkSetUri(
-        "http://localhost:8080/realms/reports-realm/protocol/openid-connect/certs").build();
+    NimbusJwtDecoder build = NimbusJwtDecoder.withJwkSetUri(
+        "http://keycloak:8080/realms/reports-realm/protocol/openid-connect/certs").build();
+    build.setJwtValidator(JwtValidators.createDefaultWithIssuer("http://localhost:8080/realms/reports-realm"));
+    return build;
   }
 
   @Bean
   public JwtAuthenticationConverter jwtAuthenticationConverter() {
     JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
     converter.setJwtGrantedAuthoritiesConverter(jwt -> {
-      List<String> roles = jwt.getClaimAsStringList("roles");
+      List<String> roles = (List<String>) ((Map)jwt.getClaim("realm_access")).get("roles");
       return roles != null ? roles.stream()
           .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
           .collect(Collectors.toList()) : List.of();
     });
     return converter;
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
--- API/src/main/java/org/example/api/controller/ReportController.java
+++ API/src/main/java/org/example/api/controller/ReportController.java
@@ -2,16 +2,21 @@ package org.example.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import java.util.logging.Logger;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReportController {
 
-  @CrossOrigin(origins = "*")
+  private static final Logger LOGGER = Logger.getLogger(ReportController.class.getName());
+
   @GetMapping("/reports")
   public List<String> getReports() {
+    LOGGER.info(
+        "API: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
     return Arrays.asList(
         "Report 1: System performance analysis",
         "Report 2: User activity summary",
